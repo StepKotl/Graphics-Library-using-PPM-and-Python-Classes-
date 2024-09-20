bbc5a8fec6c9184eaa486fb14c07d82bfc02f44c
step.kotl <stepik6544@gmail.com>
2024-09-20 10:15:46 -0400

    new file:   Photos/Background - (1792, 1120).png 	new file:   Photos/Background - 2000 x 1200.png 	new file:   Photos/Background - 720 x 1600.png 	modified:   background_generator.py 	modified:   main.py 	modified:   picture.ppm 	modified:   temp.py

--- temp.py
+++ temp.py
@@ -1,2 +1,19 @@
-for i in range(1, 2):
-    print("hi")
+from drawing import frame
+from shapes import *
+
+background = frame((1080, 1920), [245, 66, 191])
+
+objects = []
+
+scale = 220
+
+objects.append(circle((0,0), 1, (88, 233, 252), scale, 1, 1, 1))
+objects.append(circle((scale * 2,0), 1, (88, 233, 252), scale, 1, 1, 1))
+objects.append(circle((scale, int(scale * 2 - (scale / 2.4))), 1, (88, 233, 252), scale, 1, 3, 1))
+objects.append(text(()))
+
+background.draw(objects)
+
+
+
+
