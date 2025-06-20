bb63fc88c8bf57a22d2f629939994be3cb8e6f60
step.kotl <stepik6544@gmail.com>
2025-06-20 13:09:24 -0400

    Add init.py to make the library, and changed Frame to add custom directory output support

--- Classes/Frame.py
+++ Classes/Frame.py
@@ -2,12 +2,14 @@ if __name__ == "__main__":
     pass
 
 from Shapes import shape
+import os
 
 class frame ():
-    def __init__(self, size : tuple, background : list = [254, 254, 254]):
+    def __init__(self, size : tuple, background : list = [254, 254, 254], dir: str = os.getcwd()):
         self.size = size        
         self.colourmat = [[background for i in range(size[0])] for i in range(size[1])]
         self.placementmat = [[False for i in range(size[0])] for i in range(size[1])]
+        self.location = dir
 
     def setToMat(self, point : tuple, colour : list):
         self.colourmat[- point[1] - 1][point[0]] = colour
@@ -25,11 +27,12 @@ class frame ():
 
         settings = f"P3 \n{self.size[0]} {self.size[1]}\n255\n"
 
-        out = ""        
         for row in self.colourmat:
             for colour in row:
                 out += f"{colour[0]} {colour[1]} {colour[2]}\n"
 
-        with open("picture.ppm", "wb+") as colouringbook:
-            colouringbook.write((settings + out).encode("ascii"))
+        if os.path.isdir(self.location):
+            self.location = os.path.join(self.location, "picture.ppm")
 
+        with open(self.location, "wb+") as colouringbook:
+            colouringbook.write((settings + out).encode("ascii"))
